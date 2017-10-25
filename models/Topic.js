07d30b6f724802c89279df2568c564f5219ba9b5
Justin Lee <leejwg.3@gmail.com>
2017-10-24 15:13:39 -1000

    made models and data for Messages, Topics and Users, but no DB. Must build and check if code works after building DB

---
+++ models/Topic.js
@@ -0,0 +1,28 @@
+module.exports = function(sequelize, DataTypes) {
+  var Topic = sequelize.define("Topic", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    }
+  }, {
+    classMethods: {
+      associate: function(models) {
+        Topic.hasMany(models.Message, {
+          foreignKey: {
+            name: 'topic_id',
+            allowNull: false
+          }
+        });
+        Topic.belongsTo(models.User, {
+          as: 'Creator',
+          foreignKey: {
+            name: 'created_by',
+            allowNull: false
+          }
+        });
+      }
+    }
+  });
+  return Topic;
+};
--- server.js
+++ server.js
@@ -1,21 +1,26 @@
 // jshint esversion:6
 const express = require('express');
 const bodyParser = require('body-parser');
-// const db = require('./models');
-// const Message = db.message;
-// const Topic = db.topic;
-// const User = db.user;
+// db and db models
+const db = require('./models');
+const Message = db.message;
+const Topic = db.topic;
+const User = db.user;
+// ^ db models ^
+const routes = require('./routes');
 const sequelize = require('sequelize');
 const PORT = process.env.PORT || 3000;
+
+
+// express initiated
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
-
-
+app.use('/api', routes);
 
 
 const server = app.listen(PORT, () => {
-  // db.sequelize.sync({force: false});
+  db.sequelize.sync({force: false});
   console.log(`Server is Listening on port: ${PORT}`);
 });
